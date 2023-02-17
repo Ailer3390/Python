40a3b584089f11a90b40e1837abaefc4d0d5475a
Ailer3390 <ailer3390@gmail.com>
2023-02-17 19:09:55 +0700

    Base python

---
+++ main.py
@@ -0,0 +1,13 @@
+Name = input("Введите ваше имя: ")
+#age = input("Ваш возраст: ")
+#city = input("Ваш город: ")
+
+#print('Привет '+ Name+'!!!' +' Возраст: '+age +'.'" Из города: "+city+'.' )
+
+#name = 'Aleks'
+#print(dir(name))
+
+#name = 'Aleks'
+print (Name.capitalize())
+
+
