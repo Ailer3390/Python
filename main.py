9f50f370c768fbfb6870719698637c2594968799
Ailer3390 <ailer3390@gmail.com>
2023-02-18 13:45:48 +0700

    Work 23.02.2023

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-Name = input("Введите ваше имя: ")
-#age = input("Ваш возраст: ")
-#city = input("Ваш город: ")
+# Встроенные функции
+# name = input("Введите ваше имя: ")
+# age = input("Ваш возраст: ")
+# city = input("Ваш город: ")
 
-#print('Привет '+ Name+'!!!' +' Возраст: '+age +'.'" Из города: "+city+'.' )
+# print(
+#     'Привет ' + Name + '!!!' +
+#     ' Возраст: ' + age + '.' +
+#     " Из города: " + city + '.'
+#     )
 
-#name = 'Aleks'
-#print(dir(name))
+# name = 'Aleks'
+# print(dir(name))
 
-#name = 'Aleks'
-print (Name.capitalize())
-print ("Alex")
+# name = 'Aleks'
+# print (Name.split())
+# print(Name)
+
+# def my_name(name):
+#     print(name)
+
+# my_name('Aleks')
+
+# my_list = [1, 2, 3]
 
 
+# print(my_list)
+
+# a = 10
+
+
+# def my_fn():
+#     print('Hello')
+
+# This is comment
+
+# This
+# is
+# comment
+
+print('Aleks')  # Код с комментрарием
