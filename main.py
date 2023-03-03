8faeefb9baffecbfe8eda70c7b92cfd5eb95031f
Ailer3390 <ailer3390@gmail.com>
2023-03-03 15:45:01 +0700

    Copying List 3.3.2023

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 
-# СПИСОК СЛОВАРЕЙ (ИСПОЛЬЗУЕТСЯ ЧАСТО)!!!
+# КОПИРОВАНИЕ СПИСКОВ
 
-users = [
-    {
-        'user_id': 134,
-        'user_name': 'Alice'
-    },
-    {
-        'user_id': 831,
-        'user_name': 'Bob'
-    }
-]
+my_cars = ['BMW, Mercedes']
 
-print(len(users))  # 2
+# copied_cars = my_cars         # копирование списка (с добавление новых данных в оба списка)
 
-print(users[1]['user_name'])  # 831
+# copied_cars = my_cars[:]      # КОПИРОВАНИЕ В НОВЫЙ СПИСОК №1 (используя slice)
+
+# copied_cars = my_cars.copy()  # КОПИРОВАНИЕ ЧЕРЕЗ МЕТОД СПИСКОВ №2 (.copy)
+
+# copied_cars = list(my_cars)   # КОПИРОВАНИЕ ЧЕРЕЗ СОЗДАНИЕ НОВОГО СПИСКА №3 (функция list)
+
+
+copied_cars.append('Audi')
+
+print(copied_cars)
+
+print(my_cars)
+
+print(id(my_cars) == id(copied_cars))
